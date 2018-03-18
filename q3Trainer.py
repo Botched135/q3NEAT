ff5a3da9c8cd501f81e652750d23b01aecdb097d
Rasmus <Botched135>
2018-03-18 23:52:49 +0100

    Trainer now opens the process of the server

--- q3Trainer.py
+++ q3Trainer.py
@@ -11,13 +11,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str,default='./q3NN', help="path to experiment")
 parser.add_argument('--configPath', type=str,default='./configs/config-q3Trainer',help="Config-file path for neat-python algorithms")
 parser.add_argument('--init',action='store_true',help="Initiliaze training(remove previous NNs)")
-parser.add_argument('--sPath',type=str,default="../ioq3/build/release-linux-x86_64",help="path to the server file")
+parser.add_argument('--sPath',type=str,default="../ioq3/build/release-linux-x86_64/ioq3ded.x86_64",help="path to the server file")
 parser.add_argument('-s','--servers',type=int, default=1,help="Numbers of server instances")
 parser.add_argument('-t','--speed',type=float, default=10.0,help="Speed/timescale of each server")
 parser.add_argument('-g','--gLength',type=int, default=180,help="Length of each generation in seconds")
 
 args = parser.parse_args()
 
+params = (args.sPath,"+exec","server.cfg","+exec","levels.cfg","+exec","bots.cfg")
+
+popen = subprocess.Popen(params)
+popen.wait()
 
 def initialize(config_file):
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
