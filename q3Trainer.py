495629c467def9181aaa3c51c0b598c25b8b8aba
Rasmus <Botched135>
2018-03-22 16:13:22 +0100

    The pipe creation and writing to the pipe is almost done

--- q3Trainer.py
+++ q3Trainer.py
@@ -4,12 +4,28 @@ import sys
 import subprocess
 import argparse
 import neat
+import atexit
 from q3Genome import quakeGenome
+
+def OpenPipe(pipe_name):
+    if not os.path.exists(pipe_name):
+        os.mkfifo(pipe_name)
+    return open(pipe_name,'w',buffering =1)#write to the path only, 1 = line buffer. Start with writting 
+
+ 
+
+def Initialize(config_file):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+			 neat.DefaultSpeciesSet, neat.DefaultStagnation,
+		         config_file)
+
+    p = neat.Population(config)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--path", type=str,default='./q3NN', help="path to experiment")
-parser.add_argument('--configPath', type=str,default='./configs/config-q3Trainer',help="Config-file path for neat-python algorithms")
+parser.add_argument("--pipePath",'-pp', type=str, default='../pipes/pipe', help="path to pipe")
+parser.add_argument('--configPath','-cp', type=str,default='./configs/config-q3Trainer',help="Config-file path for neat-python algorithms")
 parser.add_argument('--init',action='store_true',help="Initiliaze training(remove previous NNs)")
 parser.add_argument('--sPath',type=str,default="../ioq3/build/release-linux-x86_64/ioq3ded.x86_64",help="path to the server file")
 parser.add_argument('-s','--servers',type=int, default=1,help="Numbers of server instances")
@@ -17,21 +33,27 @@ parser.add_argument('-t','--speed',type=float, default=10.0,help="Speed/timescal
 parser.add_argument('-g','--gLength',type=int, default=180,help="Length of each generation in seconds")
 
 args = parser.parse_args()
-
 params = (args.sPath,"+exec","server.cfg","+exec","levels.cfg","+exec","bots.cfg")
 
+
+#INITIALIZATION
+pipe_name = args.pipePath
+CreatePipe(pipe_name)
+initialize(args.configPath)
 popen = subprocess.Popen(params)
-popen.wait()
 
-def initialize(config_file):
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
-			 neat.DefaultSpeciesSet, neat.DefaultStagnation,
-		         config_file)
-
-    p = neat.Population(config)
+def exit_handler():
+    print('Killing off subprocesses')
+    popen.kill()
 
 
 
-
+atexit.register(exit_handler)
 if __name__ == '__main__':
-    initialize(args.configPath)
+    popen.wait()
+
+
+
+
+
+
